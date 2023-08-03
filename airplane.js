c9898abb30385fe74e4e79110b14a1370b2b4e63
hyo0o0o <hy84770@gmail.com>
2023-08-03 17:46:35 +0900

    portfolio2

---
+++ airplane.js
@@ -0,0 +1,36 @@
+(function(){
+
+    console.log("실행")
+let airplane=document.querySelector('.airplane');
+let airplaneScrollTimeline=document.querySelector('.airplane-scroll-timeline');
+
+
+airplane.animate([
+    {offsetDistance:"100%"},
+    {offsetDistance:"0%"}
+],{
+    fill: 'both',
+    timeline: new ScrollTimeline({
+        scrollOffsets: [
+           {target:airplaneScrollTimeline,edge:"start",threshold:1},
+           {target:airplaneScrollTimeline,edge:"end",threshold:1}
+        ]
+    })
+})
+
+
+
+let preScollY=0;
+
+window.addEventListener('scroll',function(){
+    if(pageYOffset >=preScollY){
+        airplane.classList.remove('up','down')
+        airplane.classList.add('down')
+    }else{
+        airplane.classList.remove('up','down')
+        airplane.classList.add('up') 
+    }
+    preScollY=pageYOffset;
+})
+
+})();
